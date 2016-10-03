fcce739e96ebaa05c6bc1c8fe78799f34248ae30
Fernando Falci <fernando.falci@charter.com>
2016-10-03 13:24:28 -0300

    PWA Sample using angular 1.x

---
+++ scripts/script.js
@@ -0,0 +1,72 @@
+angular.module('app', ['LocalStorageModule'])
+  .directive('gitCard', gitCard)
+  .service('GitService', gitService)
+  .controller('MainController', MainController);
+
+function gitService(localStorageService) {
+  var service = {
+    add: add,
+    remove: remove,
+    getAll: getAll
+  },
+
+  users = JSON.parse(localStorageService.get('users') || '[]');
+
+  return service;
+
+
+  function add(user) {
+    if(!~users.indexOf(user)){
+      users.push(user);
+      save();
+    }
+  }
+
+  function remove(user) {
+    var index = users.indexOf(user);
+
+    users.splice(index, 1);
+  }
+
+  function getAll() {
+    return users;
+  }
+
+  function save() {
+    localStorageService.set('users', JSON.stringify(users));
+  }
+}
+
+function gitCard($http) {
+  return {
+    restrict: 'E',
+    templateUrl: 'git-card.html',
+    scope: {
+      user: '='
+    },
+    link: function(scope) {
+      $http.get('https://api.github.com/users/' + scope.user)
+      .then(function (response) {
+        scope.data = response.data;
+      })
+      .catch(function (err) {
+        scope.data = {
+          name: 'user not found : ' + scope.user
+        }
+      });
+    }
+  }
+}
+
+function MainController(GitService) {
+  var vm = this;
+
+  vm.submit = submit;
+  vm.user = '';
+  vm.users = GitService.getAll();
+
+  function submit() {
+    GitService.add(vm.user);
+    vm.user = '';
+  }
+}
